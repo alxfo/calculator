5c784ceddcff85d1afb50f68500dd8a0a2b34518
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-29 15:09:59 +0800

    Make digit buttons populate display and clear button clear display

--- javascript.js
+++ javascript.js
@@ -17,3 +17,27 @@ function divide(a, b) {
 function operate(operation, a, b) {
     return operation(a, b);
 };
+
+function addToDisplay(num) {
+    displayValue += num
+    display.textContent = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = ''
+    display.textContent = displayValue;
+}
+
+const display = document.querySelector('#calc-display');
+const clearBtn = document.querySelector('.clear')
+const digitBtns = Array.from(document.querySelectorAll('.digit'));
+
+let displayValue = ''
+
+for (let i = 0; i < digitBtns.length; i++) {
+    array = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+    digitBtns[i].addEventListener('click', () => addToDisplay(array[i]))
+}
+
+clearBtn.addEventListener('click', clearDisplay)
+
