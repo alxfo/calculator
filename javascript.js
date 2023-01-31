7921c060db91ee5f369aa136ff029195f505c149
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-31 21:15:04 +0800

    Re-organise JS structure (a bit)

--- javascript.js
+++ javascript.js
@@ -1,4 +1,44 @@
-const add = (a, b) => a + b
+// DOM elements
+
+const displayCurrent = document.querySelector('#current-value');
+const displayStored = document.querySelector('#stored-values');
+const clearBtn = document.querySelector('.clear');
+const backBtn = document.querySelector('.backspace');
+const divideBtn = document.querySelector('.divide');
+const multiplyBtn = document.querySelector('.multiply');
+const subtractBtn = document.querySelector('.subtract');
+const addBtn = document.querySelector('.add');
+const decimalBtn = document.querySelector('.decimal');
+const equalsBtn = document.querySelector('.equals');
+const digitBtns = Array.from(document.querySelectorAll('.digit'));
+
+// Event listeners
+
+clearBtn.addEventListener('click', resetCalculator);
+backBtn.addEventListener('click', removeFromDisplay);
+divideBtn.addEventListener('click', divideButton);
+multiplyBtn.addEventListener('click', multiplyButton);
+subtractBtn.addEventListener('click', subtractButton);
+addBtn.addEventListener('click', addButton);
+decimalBtn.addEventListener('click', decimalButton);
+equalsBtn.addEventListener('click', calculate);
+
+for (let i = 0; i < digitBtns.length; i++) {
+    array = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+    digitBtns[i].addEventListener('click', () => addToDisplay(array[i]));
+};
+
+// Global variables
+
+let calculated = false;
+let displayValue = '';
+let numbers = [];
+let operators = [];
+let displayOperators = [];
+
+// Functions
+
+const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -9,24 +49,24 @@ function operate(operation, a, b) {
 
 function addToDisplay(num) {
     if (calculated) {
-        clearDisplay()
-        calculated = false
-    }
-    displayValue += num
+        clearDisplay();
+        calculated = false;
+    };
+    displayValue += num;
     displayCurrent.textContent = displayValue;
-}
+};
 
 function decimalButton() {
     if (displayValue.includes('.') == false) {
-        displayValue += '.'
+        displayValue += '.';
         displayCurrent.textContent = displayValue;
-    }
-}
+    };
+};
 
 function removeFromDisplay() {
     displayValue = displayValue.slice(0, -1);
     displayCurrent.textContent = displayValue;
-}
+};
 
 function resetCalculator() {
     operators = [];
@@ -34,21 +74,21 @@ function resetCalculator() {
     displayOperators = [];
     displayStored.textContent = '';
     clearDisplay();
-}
+};
 
 function clearDisplay() {
     displayValue = '';
     displayCurrent.textContent = displayValue;
-}
+};
 
 function addToStoredDisplay() {
-    combined = []
+    combined = [];
     for (i = 0; i < numbers.length; i++) {
-        combined.push(numbers[i])
-        combined.push(displayOperators[i])
-    }
+        combined.push(numbers[i]);
+        combined.push(displayOperators[i]);
+    };
     displayStored.textContent = combined.join(' ');
-}
+};
 
 function addButton() {
     numbers.push(parseFloat(displayValue));
@@ -56,7 +96,7 @@ function addButton() {
     displayOperators.push('+');
     addToStoredDisplay();
     clearDisplay();
-}
+};
 
 function divideButton() {
     numbers.push(parseFloat(displayValue));
@@ -64,7 +104,7 @@ function divideButton() {
     displayOperators.push('/');
     addToStoredDisplay();
     clearDisplay();
-}
+};
 
 function multiplyButton() {
     numbers.push(parseFloat(displayValue));
@@ -72,7 +112,7 @@ function multiplyButton() {
     displayOperators.push('x');
     addToStoredDisplay();
     clearDisplay();
-}
+};
 
 function subtractButton() {
     numbers.push(parseFloat(displayValue));
@@ -80,7 +120,7 @@ function subtractButton() {
     displayOperators.push('-');
     addToStoredDisplay();
     clearDisplay();
-}
+};
 
 function calculate() {
     numbers.push(parseFloat(displayValue));
@@ -91,43 +131,10 @@ function calculate() {
         numbers.splice(0, 2);
         operators.splice(0,1);
         numbers.unshift(parseFloat(displayValue));
-    }
+    };
     displayCurrent.textContent = displayValue;
     numbers = [];
     operators = [];
     displayOperators = [];
     calculated = true;
-}
-
-const displayCurrent = document.querySelector('#current-value');
-const displayStored = document.querySelector('#stored-values');
-const clearBtn = document.querySelector('.clear')
-const backBtn = document.querySelector('.backspace')
-const divideBtn = document.querySelector('.divide')
-const multiplyBtn = document.querySelector('.multiply')
-const subtractBtn = document.querySelector('.subtract')
-const addBtn = document.querySelector('.add')
-const decimalBtn = document.querySelector('.decimal')
-const equalsBtn = document.querySelector('.equals')
-const digitBtns = Array.from(document.querySelectorAll('.digit'));
-
-let calculated = false;
-let displayValue = '';
-let numbers = [];
-let operators = [];
-let displayOperators = [];
-
-for (let i = 0; i < digitBtns.length; i++) {
-    array = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
-    digitBtns[i].addEventListener('click', () => addToDisplay(array[i]))
-}
-
-clearBtn.addEventListener('click', resetCalculator)
-backBtn.addEventListener('click', removeFromDisplay)
-divideBtn.addEventListener('click', divideButton)
-multiplyBtn.addEventListener('click', multiplyButton)
-subtractBtn.addEventListener('click', subtractButton)
-addBtn.addEventListener('click', addButton)
-decimalBtn.addEventListener('click', decimalButton)
-equalsBtn.addEventListener('click', calculate)
-
+};
